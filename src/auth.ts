70613036f9c3c8fd1b902c0517c8f9e73b7331c5
Nwali Somto <somtobernardnwali619@gmail.com>
2024-12-23 21:46:30 +0100

    second auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./lib/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 })
